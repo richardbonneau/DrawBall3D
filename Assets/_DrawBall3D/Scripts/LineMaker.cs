c2d3e515a6faeeb9557d02977ed080d62b0ff9d6
richardbonneau <richardbonneau@hotmail.com>
2020-01-31 13:51:57 -0500

    objects instantiated but not at the right place

---
+++ Assets/_DrawBall3D/Scripts/LineMaker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineMaker : MonoBehaviour
+{
+    public GameObject line;
+    public Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButton(0))
+        {
+            Physics.Raycast(ray, out hit);
+            print(hit.point);
+            Instantiate(line, hit.point, Quaternion.identity);
+        }
+    }
+}
